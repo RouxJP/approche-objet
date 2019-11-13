8676285ea8418d2d28d2ca977fa7c766c7c7e13d
Roux JP <jproux343@gmail.com>
2019-11-13 16:48:47 +0100

    TP04 : Utilisation de méthode static existante

---
+++ src/main/java/utils/conversionNombre.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class conversionNombre {
+
+	public static void main(String[] args) {
+		String	nombreString;
+		int		nombre, a, b;
+
+		nombreString = "123";
+		nombre = Integer.valueOf(nombreString);
+		
+		System.out.println( "Le nombre en chane est " + nombreString + " en entier est : " + nombre);
+		a = 10 ;
+		b = 11;
+		System.out.println( "La valeur max de " + a + " et de " + b + " : " + Math.max( a,  b));
+	}
+
+}
