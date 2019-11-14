183c64c5cdfa48710ef217e73225d9e093643df5
Roux JP <jproux343@gmail.com>
2019-11-14 09:55:12 +0100

    TP01 : règles d'encapsulation

---
+++ src/main/java/banque/TestBanque.java
@@ -0,0 +1,13 @@
+package banque;
+import banque.entities.*;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte cpt = new Compte( "987654321", 100000);
+		
+		System.out.println("Le solde du compte " + cpt.getNumCompte() + " est " + cpt.getSoldeCompte() );
+
+	}
+
+}
---
+++ src/main/java/banque/entities/Compte.java
@@ -0,0 +1,24 @@
+package banque.entities;
+
+public class Compte {
+	String	numCompte ;
+	double	soldeCompte ;
+	public Compte( String numCompte, double soldeCompte) {
+		this.numCompte 		= numCompte ;
+		this.soldeCompte 	= soldeCompte ;
+	}
+	public String getNumCompte() {
+		return numCompte;
+	}
+	public void setNumCompte(String numCompte) {
+		this.numCompte = numCompte;
+	}
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+	public void setSoldeCompte(double soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+	
+	
+}
