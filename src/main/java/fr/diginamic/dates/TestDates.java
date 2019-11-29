6e0eeb6b6da5cfc35f8257bf4e64d81d1068a6c7
Roux JP <jproux343@gmail.com>
2019-11-29 15:03:49 +0100

    TP : Date and Time correctif exo 2

--- src/main/java/fr/diginamic/dates/TestDates.java
+++ src/main/java/fr/diginamic/dates/TestDates.java
@@ -1,27 +1,27 @@
 package fr.diginamic.dates;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class TestDates {
 
 	public static void main(String[] args) {
 		Date d1 = new Date();
-		SimpleDateFormat formatteur = new SimpleDateFormat( "dd/mm/yyyy HH:mm:ss");
+		SimpleDateFormat formatteur = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss");
 		System.out.println( formatteur.format( d1));
 		
 		
-		Date d2 = new Date(2019, 11, 20, 23, 59, 00);
+		Date d2 = new Date(119, 11, 20, 23, 59, 00);	/* 117 pour 2017 */
 		System.out.println( formatteur.format( d2));
-/*
+
 		Calendar cal = Calendar.getInstance();
-		cal.set( Calendar.HOUR, 23);
+		cal.set( Calendar.HOUR, 11);   					/* 12 + x */
 		cal.set( Calendar.MINUTE, 59);
 		cal.set( Calendar.SECOND, 00);
 		
-		Date d2 = cal.getTime();
-		System.out.println(formatteur.format( d2));
-*/		
+		Date d3 = cal.getTime();
+		System.out.println(formatteur.format( d3));	
 	}
 
 }
